0deeed9df8eea7828407ad02c300fd754067b42a
raycho <raycho521@yonsei.ac.kr>
2023-02-26 10:21:37 +0900

    feat: add initial settings

---
+++ src/main/java/com/example/springs3practice/SpringS3PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springs3practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS3PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringS3PracticeApplication.class, args);
+    }
+
+}
